21708024f238dc2a1fdb837176829012f2d86003
Diego Rodriguez <diego.rodriguez@datatonic.com>
2021-05-21 14:07:57 +0200

    Changes in covid19_open_data schema

--- examples/service_project/embedded_dop/orchestration/example_covid19/sql/stg_covid19.sql
+++ examples/service_project/embedded_dop/orchestration/example_covid19/sql/stg_covid19.sql
@@ -1,6 +1,7 @@
 with covic_cases as (
 
-    SELECT * REPLACE(CAST(new_recovered AS INT64) as new_recovered) FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
+    SELECT * REPLACE(CAST(new_recovered AS INT64) as new_recovered, CAST(new_tested AS INT64) as new_tested)
+    FROM `bigquery-public-data.covid19_open_data.covid19_open_data`
     {% if is_incremental() %}
         where date >= DATE("{{ ds }}")
     {% endif %}
